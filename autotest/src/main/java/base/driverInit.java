b19c3ba1a9c992f28e3ae2cbec0e275276e0f88d
zhangxiaochun <401506606@qq.com>
2020-01-10 09:30:19 +0800

    基础

---
+++ autotest/src/main/java/base/driverInit.java
@@ -0,0 +1,10 @@
+package base;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class driverInit {
+    ChromeDriver driver;
+    public void driverInit(){
+        Webdriver driver = new ChromeDriver("");
+    }
+}
--- autotest/src/main/java/com/example/one/MyFirstJavaClass.java
+++ autotest/src/main/java/com/example/one/MyFirstJavaClass.java
@@ -2,4 +2,5 @@ package com.example.one;
 
 public class MyFirstJavaClass {
 
+
 }

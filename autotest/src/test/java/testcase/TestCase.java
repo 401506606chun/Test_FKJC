14ce4eaba327e4d044df5630b03f0e126c9eb1a5
zhangxiaochun <401506606@qq.com>
2020-01-14 15:19:49 +0800

    增加test

--- autotest/src/test/java/testcase/TestCase.java
+++ autotest/src/test/java/testcase/TestCase.java
@@ -4,6 +4,7 @@ import base.DriverBase;
 import constant.Constant;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
@@ -14,24 +15,37 @@ public class TestCase {
     LoginPage loginPage = new LoginPage(driver);
 
 
-    public void signIn(String url) {
+    public void signIn(String url,String user,String password) {
 //       打开登录页面
         loginPage.gotoUrl(url);
 //        输入用户名 user
-        loginPage.getUserElement().sendKeys("admin");
+        loginPage.getUserElement().sendKeys(user);
 //        输入密码 password
-        loginPage.getPasswordElement().sendKeys("123456");
+        loginPage.getPasswordElement().sendKeys(password);
 //        点击登录 loginbutton
         loginPage.getLoginbuttonElement().click();
 
     }
-    @Test
-    public void testLogin(){
-        this.signIn(Constant.loginUrl);
+    public boolean  loginResult(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         boolean isLogin = false;
 //        出现退出按钮 证明登录成功
-        isLogin = driver.findElement(By.xpath("//a[text()=' 退出']")).isDisplayed();
-        Assert.assertTrue(isLogin);
+        WebElement logout = driver.findElement(By.xpath("//a[text()=' 退出']"));
+        return logout.isDisplayed();
+    }
+    @Test
+    public void testLoginSuccess(){
+        this.signIn(Constant.loginUrl,"admin","123456");
+        Assert.assertTrue(this.loginResult());
+    }
+    @Test
+    public void testLoginError(){
+        this.signIn(Constant.loginUrl,"admin","1234567");
+        Assert.assertTrue(this.loginResult());
     }
 //    public static void main(String args[]) {
 //        TestCase testCase = new TestCase();
